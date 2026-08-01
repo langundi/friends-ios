3ce41e00e29ea882a60ddcd5edad1194d1fa183f
raziqaizza <raziqaizza@gmail.com>
2026-08-01 11:27:07 +0700

    added profile screen

--- Friends/App/Views/Profile/ProfileScreen.swift
+++ Friends/App/Views/Profile/ProfileScreen.swift
@@ -11,10 +11,48 @@ struct ProfileScreen: View {
     @Environment(AppRouter.self) var router
     @State private var viewmodel = ProfileViewModel()
     
+    private var posts = 17
+    private var columns = Array(repeating: GridItem(.flexible(), spacing: 12), count: 3)
+    
     var body: some View {
-        VStack {
-            Text("Profile Screen")
+        ScrollView {
+            HStack(spacing: 24) {
+                Circle()
+                    .frame(maxWidth: 60, maxHeight: 60)
+                
+                HStack(alignment: .center, spacing: 16) {
+                    Text("\(posts) Posts")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    Text("5 Friends")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                }
+                
+                Spacer(minLength: 0)
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+            
+            LazyVGrid(
+                columns: columns,
+                alignment: .center,
+                spacing: 12
+            ) {
+                ForEach(0..<posts, id: \.self) { column in
+                    Rectangle()
+                        .frame(maxWidth: .infinity)
+                        .aspectRatio(1.0, contentMode: .fit)
+                        .foregroundStyle(.gray.opacity(0.15))
+                }
+            }
+            .padding([.leading, .trailing])
+            .padding(.bottom, 16)
         }
+        .scrollIndicators(.hidden)
+        .navigationTitle("@username")
+        .toolbarTitleDisplayMode(.inlineLarge)
         .toolbar {
             ToolbarItemGroup(placement: .topBarTrailing) {
                 Button {
