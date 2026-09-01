725cec92e8772b82dc63d89973e5f3339b82e0c0
raziqaizza <raziqaizza@gmail.com>
2026-09-01 21:46:24 +0700

    added check for load more button

--- Friends/App/Views/Timeline/TimelineStackView.swift
+++ Friends/App/Views/Timeline/TimelineStackView.swift
@@ -14,6 +14,15 @@ struct TimelineStackView: View {
     @Environment(AppRouter.self) var router
     var viewModel: TimelineViewModel
     var posts: [PostResponse]
+    @State private var scrolledID: Int?
+    @State private var showLoadMore: Bool = false
+    
+    private var currentCreatedAt: Date {
+        guard let post = posts.first(where: { $0.id == scrolledID }) else {
+            return Date()
+        }
+        return post.createdAt
+    }
     
     init(viewModel: TimelineViewModel, posts: [PostResponse]) {
         self.viewModel = viewModel
@@ -53,10 +62,40 @@ struct TimelineStackView: View {
                 .ignoresSafeArea(edges: [.horizontal, .bottom])
                 .scrollTargetBehavior(.paging)
                 .scrollIndicators(.hidden)
+                .scrollPosition(id: $scrolledID)
+                .overlay(alignment: .bottom) {
+                    if showLoadMore {
+                        Button {
+                            // load more posts
+                        } label: {
+                            Label("Load More", systemImage: "arrow.down")
+                                .padding()
+                        }
+                        .buttonStyle(ToolbarButtonStyle())
+                        .transition(.blurReplace)
+                        .padding(.bottom)
+                    }
+                }
+                .onChange(of: scrolledID) { oldValue, newValue in
+                    checkForLoadMore(currentID: newValue)
+                }
                 .refreshable {
                     await viewModel.refreshTimeline()
                 }
+                .animation(.snappy, value: showLoadMore)
             }
+        }
+    }
+    
+    private func checkForLoadMore(currentID: Int?) {
+        guard let currentID, let index = posts.firstIndex(where: { $0.id == currentID }) else {
+            return
+        }
+        
+        if index == posts.count - 1 {
+            showLoadMore = true
+        } else {
+            showLoadMore = false
         }
     }
 }
